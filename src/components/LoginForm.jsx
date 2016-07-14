1af7cff7da0aba91e3cc6aa75c770e8bcdd092f0
Jeremy Chung <jchung@vistaprint.com>
2016-07-14 13:52:14 -0400

    RC-2: Create basic form for login purposes

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,10 +1,53 @@
 import React, {Component} from 'react';
 
 export default class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {username: '', password: ''};
+    this.onUsernameChangeHandler = this.onUsernameChangeHandler.bind(this);
+    this.onPasswordChangeHandler = this.onPasswordChangeHandler.bind(this);
+    this.onLogin = this.onLogin.bind(this);
+  }
+  onUsernameChangeHandler(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+  onPasswordChangeHandler(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+  onLogin(e) {
+    e.preventDefault();
+    this.setState({
+      password: ''
+    });
+  }
   render() {
+    var disabled = <input type='submit' value='Login' disabled />;
+    var enabled = <input type='submit' value='Login' />;
+    var display = disabled;
+
+    if (this.state.username && this.state.password) {
+      display = enabled;
+    }
+
     return (
       <div>
-        Hello World!
+        <h1>Welcome to KatChat!</h1>
+        <br/>
+        <form onSubmit={this.onLogin}>
+          <input type='text' value={this.state.username} onChange={this.onUsernameChangeHandler} />
+          <br/>
+          username
+          <br/><br/>
+          <input type='password' value={this.state.password} onChange={this.onPasswordChangeHandler} />
+          <br/>
+          password
+          <br/><br/>
+          {display}
+        </form>
       </div>
     );
   }
